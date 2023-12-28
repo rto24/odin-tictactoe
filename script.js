8ce780eb9c187ba0837ef8a78f706f68a62cde9e
Richard <richardto180@gmail.com>
2023-12-28 15:06:02 -0500

    Comment out previous code, added factory function for gameBoard

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
-function Gameboard() {
-  const rows = 3;
-  const columns = 3;
+// function Gameboard() {
+//   const rows = 3;
+//   const columns = 3;
+//   const board = [];
+
+//   for (let i = 0; i < rows; i++) {
+//     board[i] = [];
+//     for (let j =0; j < columns; j++) {
+//       board[i].push(Cell());
+//     }
+//   }
+
+//   const retrieveBoard = () => board;
+
+//   const placeCharacter = (column, player) => {
+//     const openCells = board.filter((row) =>
+//     row[column].getValue() === 0).map(row => row[column]);
+//     if (!openCells.length) return;
+//     const openRow = openCells.length;
+//     board[openRow][column].placeItem(player);
+//   };
+
+//   const printBoard = () => {
+//     const getBoardCellValues = board.map((row) => row.map((cell) =>
+//     cell.getValue()))
+//     console.log(getBoardCellValues);
+//   };
+//   return { retrieveBoard, placeCharacter, printBoard };
+// }
+
+// function Cell() {
+//   let value = 0;
+//   const placeItem = (player) => {
+//     value = player;
+//   };
+
+//   const getValue = () => value;
+
+//   return {
+//     placeItem,
+//     getValue
+//   };
+// }
+
+const gameBoard = (() => {
   const board = [];
 
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j =0; j < columns; j++) {
-      board[i].push(Cell());
-    }
-  }
-
-  const retrieveBoard = () => board;
-
-  const placeCharacter = (column, player) => {
-    const openCells = board.filter((row) =>
-    row[column].getValue() === 0).map(row => row[column]);
-    if (!openCells.length) return;
-    const openRow = openCells.length;
-    board[openRow][column].placeItem(player);
+  const playerGroup = (name, marker, turn) => {
+    return { name, marker, turn};
   };
+// Players
+  const player1 = playerGroup('player 1', 'X', true);
+  const player2 = playerGroup('player 2', 'O', false);
 
-  const printBoard = () => {
-    const getBoardCellValues = board.map((row) => row.map((cell) =>
-    cell.getValue()))
-    console.log(getBoardCellValues);
-  };
-  return { retrieveBoard, placeCharacter, printBoard };
-}
-
-function Cell() {
-  let value = 0;
-  const placeItem = (player) => {
-    value = player;
-  };
-
-  const getValue = () => value;
-
-  return {
-    placeItem,
-    getValue
-  };
-}
+})();
