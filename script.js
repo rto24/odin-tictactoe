98e579749441c7eb0b682a47809d39136317eefe
Richard <richardto180@gmail.com>
2023-12-28 17:43:24 -0500

    Added tie message and removed commented out code

--- script.js
+++ script.js
@@ -1,47 +1,3 @@
-// function Gameboard() {
-//   const rows = 3;
-//   const columns = 3;
-//   const board = [];
-
-//   for (let i = 0; i < rows; i++) {
-//     board[i] = [];
-//     for (let j =0; j < columns; j++) {
-//       board[i].push(Cell());
-//     }
-//   }
-
-//   const retrieveBoard = () => board;
-
-//   const placeCharacter = (column, player) => {
-//     const openCells = board.filter((row) =>
-//     row[column].getValue() === 0).map(row => row[column]);
-//     if (!openCells.length) return;
-//     const openRow = openCells.length;
-//     board[openRow][column].placeItem(player);
-//   };
-
-//   const printBoard = () => {
-//     const getBoardCellValues = board.map((row) => row.map((cell) =>
-//     cell.getValue()))
-//     console.log(getBoardCellValues);
-//   };
-//   return { retrieveBoard, placeCharacter, printBoard };
-// }
-
-// function Cell() {
-//   let value = 0;
-//   const placeItem = (player) => {
-//     value = player;
-//   };
-
-//   const getValue = () => value;
-
-//   return {
-//     placeItem,
-//     getValue
-//   };
-// }
-
 const gameBoard = (() => {
   const board = [];
   let turns = 0;
@@ -136,6 +92,8 @@ const display = (() => {
     console.log(winText.textContent)
   } else if (gameBoard.winner === 'player2') {
     winText.textContent = 'Player 2 Wins!';
+  } else if (gameBoard.winner === 'tie') {
+    winText.textContent = `It's a tie!`;
   } else {
     return;
   }
